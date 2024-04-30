5c9d26fd0444ed1509e7626ec370938f308a1153
Emanuel Peire <emapeire@outlook.com>
2024-04-30 19:37:50 -0300

    Update server port configuration and add database configuration in .env.example

--- src/config/index.js
+++ src/config/index.js
@@ -1,8 +1,13 @@
 import 'dotenv/config'
 
 export const config = {
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  host: process.env.DB_HOST || 'localhost',
-  port: process.env.DB_PORT || 3306
+  mysqlConfig: {
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT || 3306
+  },
+  serverConfig: {
+    port: process.env.PORT || 3000
+  }
 }
--- src/server/index.js
+++ src/server/index.js
@@ -3,16 +3,17 @@ import mysql from 'mysql2/promise'
 import mongoose from 'mongoose'
 import { config } from '../config/index.js'
 
-const { user, password, host, port } = config
+const { mysqlConfig, serverConfig } = config
 
+// Create an express app
 const app = express()
 
 // Connect to MySQL
 const client = mysql.createPool({
-  host,
-  port,
-  user,
-  password
+  host: mysqlConfig.host,
+  port: mysqlConfig.port,
+  user: mysqlConfig.user,
+  password: mysqlConfig.password
 })
 
 // A simple query to test the connection
@@ -23,6 +24,10 @@ console.log(response)
 const mongo = await mongoose.connect('mongodb://localhost:27017/dockerdb')
 console.log('MongoDB connected', mongo.connection.db.databaseName)
 
-app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000')
+app.get('/', (_req, res) => {
+  res.send('Hello, world!')
+})
+
+app.listen(serverConfig.port, () => {
+  console.log(`Server is running on http://localhost:${serverConfig.port}`)
 })
