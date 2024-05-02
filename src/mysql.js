986792f71d4dcfb388dce609c9ac54b692adb133
Emanuel Peire <emapeire@outlook.com>
2024-05-01 22:44:35 -0300

    Refactor MySQL table creation and insertion in mysql.js

--- src/mysql.js
+++ src/mysql.js
@@ -2,7 +2,7 @@ import { createPool } from 'mysql2/promise'
 
 export const connectMySQL = async () => {
   const mysql = createPool({
-    host: 'localhost' || 'docker-mysql',
+    host: 'localhost' ?? 'docker-mysql',
     user: 'root',
     password: 'pass',
     port: 3306,
@@ -10,21 +10,22 @@ export const connectMySQL = async () => {
   })
   console.log('✅ MySQL connected')
 
+  await mysql.query(`DROP TABLE IF EXISTS products`)
+
   await mysql.query(
     `CREATE TABLE IF NOT EXISTS products (
       id INT AUTO_INCREMENT PRIMARY KEY,
       name VARCHAR(255) NOT NULL,
-      price INT NOT NULL
+      price INT NOT NULL,
+      UNIQUE INDEX idx_name_price (name, price)
     )`
   )
 
   await mysql.query(
     `INSERT INTO products (name, price)
-    SELECT * FROM (SELECT ? AS name, ? AS price) AS tmp
-    WHERE NOT EXISTS (
-      SELECT name FROM products WHERE name = ? AND price = ?
-    ) LIMIT 1;`,
-    ['Product 2', 200, 'Product 2', 200]
+    VALUES (?, ?)
+    ON DUPLICATE KEY UPDATE id = id;`,
+    ['Product 2', 200]
   )
 
   const [mysqlProduct] = await mysql.query('SELECT * FROM products')
